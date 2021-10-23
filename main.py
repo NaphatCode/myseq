90848bc45cf7b300613530625332bf60e08fb900
NaphatCode <naphat.spakdee@gmail.com>
2021-10-23 22:17:07 +0700

    update

---
+++ main.py
@@ -0,0 +1,5 @@
+#from seqbio.calculation.SeqCal import gcContent,countBases
+#from seqbio.seqMan.dnaconvert import transcription,translation
+#from seqbio.pattern.SeqPattern import enzTargetScan 
+def main():
+    print("main can run")
